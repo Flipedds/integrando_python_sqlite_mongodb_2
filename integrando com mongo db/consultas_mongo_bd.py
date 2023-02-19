15be0ddac46fb307f6e1fbe528316ce145e52422
Filipe André <flipeandre12@gmail.com>
2023-02-19 20:18:54 -0300

    atualiza arquivos python

--- integrando com mongo db/consultas_mongo_bd.py
+++ integrando com mongo db/consultas_mongo_bd.py
@@ -1,12 +1,47 @@
 import pprint
 
 import pymongo as pyM
-# conexão omitada por questões de segurança
+from bson import ObjectId
+
+# conecta ao mongodb
 cliente = pyM.MongoClient(cliente)
 
 db = cliente.test
 collection = db.test_collection
 
-print('\n Procurar por campo')
-pprint.pprint(db.clientes.find_one({'nome': 'filipe'}))
+# print('\n Procurar por campo')
+# pprint.pprint(db.clientes.find_one({'nome': 'filipe'}))
 
+# Outras formas de procurar por _id
+# objInstance = ObjectId(id)
+# print(db.clientes.find_one({"_id": objInstance}))
+# print(db.clientes.find_one({"_id": ObjectId(id)}))
+
+# procurar cliente por _id
+id = "63f27486b468c5993e20f01b"
+
+consulta = db.clientes.find_one(ObjectId(id))
+if consulta is None:
+    print('Não foi possível encontrar esse cliente')
+print(f"""
+        Id: {consulta['_id']}
+        Nome: {consulta['nome']}
+        Cpf: {consulta['cpf']}
+        Endereço: {consulta['endereco']}
+""")
+
+id_user = "63f27486b468c5993e20f01b"
+
+resultado = db.contas.find_one({'id_cliente': ObjectId(id_user)})
+
+if resultado is None:
+    print('Não foi possível encontrar essa conta')
+else:
+    print(f"""
+        Id: {resultado['_id']}
+        Tipo: {resultado['tipo']}
+        Agência: {resultado['agencia']}
+        Numero: {resultado['num']}
+        Id_Cliente: {resultado['id_cliente']}
+        Saldo: {resultado['saldo']}
+    """)
--- integrando com mongo db/integra_mongodb.py
+++ integrando com mongo db/integra_mongodb.py
@@ -1,20 +1,24 @@
 import pymongo as pyM
 
-# conecta ao mongodb
-# conexão omitada por questões de segurança
-cliente = pyM.MongoClient(cliente)
 
+# conecta ao mongodb
+cliente = pyM.MongoClient(cliente)
+# Abrir banco de dados
 db = cliente.test
 collection = db.test_collection
 
+# Criar cliente
 cliente = {
     "nome": "filipe",
     "cpf": "123.123.123-23",
     "endereco": "rua ana 123",
 }
+
+# cria a coleção clientes e adicione-lhe o documento
 clientes = db.clientes
 cliente_id = clientes.insert_one(cliente).inserted_id
 
+# Criar conta
 conta = {
     "tipo": "corrente",
     "agencia": "12.123-12",
@@ -22,6 +26,7 @@ conta = {
     "id_cliente": cliente_id,
     "saldo": 150.00,
 }
+# cria a coleção contas e adicione-lhe o documento
 contas = db.contas
 conta_id = contas.insert_one(conta).inserted_id
 
